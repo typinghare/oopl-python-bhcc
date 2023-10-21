1a480f1995d83aada8d44f213dafb704a3b17e2f
James Chan <jameschan312.cn@gmail.com>
2023-10-21 19:59:58 -0400

    Oct 21, 2023

--- src/homework/5/breaking_news.py
+++ src/homework/5/breaking_news.py
@@ -1,23 +1,36 @@
 """Publisher module."""
 from typing import List
 from subscriber import Subscriber
+from abc import ABC, abstractmethod
 
 
-class Publisher:
-    def __init__(self):
-        # Subscribers
-        self._subscriber_list: List[Subscriber] = []
+class Publisher(ABC):
+    """Abstract publisher."""
 
+    @abstractmethod
     def attach(self, subscriber):
         """Attach a subscriber."""
-        self._subscriber_list.append(subscriber)
 
+    @abstractmethod
     def detach(self, subscriber):
         """Detach a subscriber."""
-        self._subscriber_list.remove(subscriber)
+
+    @abstractmethod
+    def notify(self, news_category, news_title):
+        pass
 
 
 class NewYorkTimesBreakingNews(Publisher):
+    def __init__(self):
+        # Subscribers
+        self._subscribers: List[Subscriber] = []
+
+    def attach(self, subscriber):
+        self._subscribers.append(subscriber)
+
+    def detach(self, subscriber):
+        self._subscribers.remove(subscriber)
+
     def publish_news_item(self, news_category, news_title):
         """Publish a piece of news."""
         print(f"NYT: Breaking News: {news_title} [category={news_category}]")
@@ -26,5 +39,5 @@ class NewYorkTimesBreakingNews(Publisher):
     def notify(self, news_category, news_title):
         """Notify all subscribers about a piece of news."""
         print(f"NYT: Notifying subscribers...")
-        for subscriber in self._subscriber_list:
+        for subscriber in self._subscribers:
             subscriber.breaking_news(news_category, news_title)
